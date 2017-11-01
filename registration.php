bd99fffa9682dd2ab5dc2246f2237e39294b418b
php-cuong <bestearnmoney87@gmail.com>
2017-11-02 01:42:43 +0700

    update

--- registration.php
+++ registration.php
@@ -4,12 +4,12 @@
  * @Author: Ngo Quang Cuong
  * @Date:   2017-07-11 03:54:20
  * @Last Modified by:   https://www.facebook.com/giaphugroupcom
- * @Last Modified time: 2017-11-02 01:19:42
+ * @Last Modified time: 2017-11-02 01:42:22
  * @website: http://giaphugroup.com
  */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'php-cuong_fr_fr',
+    'phpcuong_fr_fr',
     __DIR__
 );
